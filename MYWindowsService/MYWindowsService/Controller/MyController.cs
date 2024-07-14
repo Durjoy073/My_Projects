9c8e9bcd7b8b19b257fbd164a83cff27d14f6f1c
Durjoy Nath <durjoynathcse073@gmail.com>
2024-07-14 13:22:31 +0600

    WindowsService with hosting API in same project

---
+++ MYWindowsService/MYWindowsService/Controller/MyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYWindowsService.Controllers;
+[Microsoft.AspNetCore.Mvc.Route("[controller]")]
+[ApiController]
+
+public class MyController:ControllerBase
+{
+    [HttpGet]
+    public ContentResult Get()
+    {
+        string somecontent = "This is for Api Hosting";
+        return new ContentResult
+        {
+            Content = somecontent,
+            ContentType = "text/html"
+
+        };
+
+
+       }
+
+  }
+
---
+++ MYWindowsService/MYWindowsService/Program.cs
@@ -0,0 +1,24 @@
+using MYWindowsService;
+using Microsoft.AspNetCore.Builder;
+//var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddWindowsService();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var host = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (host.Environment.IsDevelopment())
+{
+    host.UseSwagger();
+    host.UseSwaggerUI();
+}
+
+host.MapControllers();
+host.Run();
